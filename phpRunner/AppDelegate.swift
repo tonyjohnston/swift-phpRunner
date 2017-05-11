7341b287be14e51bbabb272aff4205f1d5e0c64c
Victoria Clipper <tjohnston@victoriaclipper.com>
2017-05-11 09:18:25 -0700

    Initial Commit

---
+++ phpRunner/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  phpRunner
+//
+//  Created by Tony Johnston VC on 5/11/17.
+//  Copyright © 2017 Clipper Vacations. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ phpRunner/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  phpRunner
+//
+//  Created by Tony Johnston VC on 5/11/17.
+//  Copyright © 2017 Clipper Vacations. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
